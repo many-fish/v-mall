460ffb451f23e19a37440cf1587311b1f05b7f81
wangwei <jike_wangwei@163.com>
2019-11-01 10:34:40 +0800

    测试

--- src/pages/goods/good-sort.vue
+++ src/pages/goods/good-sort.vue
@@ -4,6 +4,10 @@
             <van-sidebar-item title="标签名称" />
             <van-sidebar-item title="标签名称" />
             <van-sidebar-item title="标签名称" />
+            <van-sidebar-item title="标签名称" />
+            <van-sidebar-item title="标签名称" />
+            <van-sidebar-item title="标签名称" />
+            <van-sidebar-item title="标签名称" />
         </van-sidebar>
 
         <van-tree-select
